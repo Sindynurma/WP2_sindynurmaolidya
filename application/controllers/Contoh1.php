f304ec71d9ab20791defbf6e6ba4922c6fb74694
vvandriani <andrianififi75@gmail.com>
2021-10-28 21:15:34 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan<h1>";
+		echo "Nama saya Sindy Nurmaolidya dari kelas 12.3A.15 Program Studi Sistem Informasi";
+	}
+}
